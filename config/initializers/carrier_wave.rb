ce6794a32513b0ae994fe8519f0c7e7960af13f3
Javier Machin <javier_machin_acosta@hotmail.com>
2018-05-12 13:04:56 +0100

    Change S3 region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
+      :region => ENV['eu-west-2']
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
+
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
