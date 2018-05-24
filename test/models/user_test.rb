99eaa2611270a44d58cb670f04ce35837e60c4fd
Javier Machin <javier_machin_acosta@hotmail.com>
2018-05-24 14:13:54 +0100

    Add following, followers and followed models, relations and tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -83,6 +83,17 @@ class UserTest < ActiveSupport::TestCase
     assert_difference 'Micropost.count', -1 do 
       @user.destroy
     end
+  end
+
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
   end 
   
 end
