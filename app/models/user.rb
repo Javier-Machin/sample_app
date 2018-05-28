f1c4962504c29fb5b92d82f567b61173a4b71325
Javier Machin <javier_machin_acosta@hotmail.com>
2018-05-28 12:07:59 +0100

    Improve home page status feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -74,9 +74,13 @@ class User < ApplicationRecord
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
-
+  
+  # Returns a user's status feed.
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
    # Follows a user.
